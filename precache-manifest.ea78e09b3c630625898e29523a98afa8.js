32f9bd8dae9c3eeb87d15c978a20b3ded263435b
Artem Barshatskyi <macbook@Artems-MacBook.local>
2020-03-24 18:24:48 +0200

    Updates

--- precache-manifest.ea78e09b3c630625898e29523a98afa8.js
+++ precache-manifest.ea78e09b3c630625898e29523a98afa8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b45d8200557423d62b809fc3fb306807",
+    "revision": "cf86c0b5d68d40fc793579d387e0d717",
     "url": "/index.html"
   },
   {
-    "revision": "ef9c99d441fcee40d01f",
+    "revision": "9d3274699a3fa13f4ed3",
     "url": "/static/css/main.3fc0fc16.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.662cd849.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef9c99d441fcee40d01f",
-    "url": "/static/js/main.298f65c6.chunk.js"
+    "revision": "9d3274699a3fa13f4ed3",
+    "url": "/static/js/main.567cde2a.chunk.js"
   },
   {
     "revision": "e2e56ea2690a012280da",
