cee883590db1cb496b8b1903b7a45c2a8b7423ed
Artem Barshatskyi <macbook@Artems-MacBook.local>
2020-03-25 21:59:35 +0200

    Updates

---
+++ precache-manifest.1d80e71731a31993451c6f809520837d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bafa60151dabe798ba27901ec7ed902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7682c89e1f3fe85a9495",
+    "url": "/static/css/main.cfd474f4.chunk.css"
+  },
+  {
+    "revision": "713712c43d7518cbc4c4",
+    "url": "/static/js/2.3d1ebae0.chunk.js"
+  },
+  {
+    "revision": "45ee638b75c887902f079222be463d38",
+    "url": "/static/js/2.3d1ebae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7682c89e1f3fe85a9495",
+    "url": "/static/js/main.4e4dbe98.chunk.js"
+  },
+  {
+    "revision": "e2e56ea2690a012280da",
+    "url": "/static/js/runtime-main.7b347686.js"
+  },
+  {
+    "revision": "b1b468f66000215161bb5b09061b69a1",
+    "url": "/static/media/virus.b1b468f6.svg"
+  }
+]);
